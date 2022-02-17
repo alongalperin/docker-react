45ad70b6024ab04b65fea511b2ead4ac5b5d76af
Alon <alonga90@gmail.com>
2022-02-17 13:56:05 +0200

    Change app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React111111111
+          Learn React111111111 I was changed on the feature branch
         </a>
       </header>
     </div>
