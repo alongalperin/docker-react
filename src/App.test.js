9e9f58b1d2e7db54f988de3fec7927032faa5be8
Alon <alonga90@gmail.com>
2022-02-17 14:02:50 +0200

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,3 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test("renders learn react link", () => {
-  expect(1).toEqual(2);
-});
